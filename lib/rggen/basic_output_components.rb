7f8267a3313344d7cee0938a54cc14786b9f5306
taichi <taichi730@gmail.com>
2019-02-28 14:26:03 +0900

    added 'frozen-string-literal: true' magic comment

--- lib/rggen/basic_output_components.rb
+++ lib/rggen/basic_output_components.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'basic_output_components/version'
 
 require_relative 'basic_output_components/code_utility/line'
--- lib/rggen/basic_output_components/code_utility/line.rb
+++ lib/rggen/basic_output_components/code_utility/line.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module RgGen
   module BasicOutputComponents
     module CodeUtility
