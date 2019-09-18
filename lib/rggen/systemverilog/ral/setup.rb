55a1ad5d9de9de4ec0c48ac0f69b0b2944e0304e
taichi <taichi730@gmail.com>
2019-09-18 15:25:35 +0900

    changed plugin name according to rggen/rggen#10

--- lib/rggen/systemverilog/ral/setup.rb
+++ lib/rggen/systemverilog/ral/setup.rb
@@ -2,6 +2,6 @@
 
 require 'rggen/systemverilog/ral'
 
-RgGen.setup :'sv-ral', RgGen::SystemVerilog::RAL do |builder|
+RgGen.setup :'rggen-sv-ral', RgGen::SystemVerilog::RAL do |builder|
   builder.enable :register_block, [:sv_ral_package]
 end
