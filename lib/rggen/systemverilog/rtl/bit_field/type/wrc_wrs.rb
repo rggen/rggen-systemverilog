627ab15212b44150d13c6eb0e01fc1bdd8385217
Taichi Ishitani <taichi730@gmail.com>
2020-08-21 23:54:45 +0900

    added wrc and wrs types

---
+++ lib/rggen/systemverilog/rtl/bit_field/type/wrc_wrs.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+RgGen.define_list_item_feature(:bit_field, :type, [:wrc, :wrs]) do
+  sv_rtl do
+    build do
+      output :value_out, {
+        name: "o_#{full_name}", data_type: :logic, width: width,
+        array_size: array_size, array_format: array_port_format
+      }
+    end
+
+    main_code :bit_field, from_template: true
+  end
+end
