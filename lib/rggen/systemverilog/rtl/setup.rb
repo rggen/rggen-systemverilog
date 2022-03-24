87022fc1b0c35f9818b9597bf3f7eaf89da10a5a
Taichi Ishitani <taichi730@gmail.com>
2022-03-24 22:33:39 +0900

    add wishbone to setup file (#73)
    (refs: rggen/rggen#89)

--- lib/rggen/systemverilog/rtl/setup.rb
+++ lib/rggen/systemverilog/rtl/setup.rb
@@ -5,7 +5,7 @@ require 'rggen/systemverilog/rtl'
 RgGen.register_plugin RgGen::SystemVerilog::RTL do |builder|
   builder.enable :global, [:array_port_format]
   builder.enable :register_block, [:sv_rtl_top, :protocol]
-  builder.enable :register_block, :protocol, [:apb, :axi4lite]
+  builder.enable :register_block, :protocol, [:apb, :axi4lite, :wishbone]
   builder.enable :register_file, [:sv_rtl_top]
   builder.enable :register, [:sv_rtl_top]
   builder.enable :bit_field, [:sv_rtl_top]
--- spec/rggen/systemverilog/rtl_spec.rb
+++ spec/rggen/systemverilog/rtl_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe RgGen::SystemVerilog::RTL do
       expect(RgGen::SystemVerilog::RTL.plugin_spec).to receive(:activate).with(equal(builder))
       expect(builder).to receive(:enable).with(:global, [:array_port_format])
       expect(builder).to receive(:enable).with(:register_block, [:sv_rtl_top, :protocol])
-      expect(builder).to receive(:enable).with(:register_block, :protocol, [:apb, :axi4lite])
+      expect(builder).to receive(:enable).with(:register_block, :protocol, [:apb, :axi4lite, :wishbone])
       expect(builder).to receive(:enable).with(:register_file, [:sv_rtl_top])
       expect(builder).to receive(:enable).with(:register, [:sv_rtl_top])
       expect(builder).to receive(:enable).with(:bit_field, [:sv_rtl_top])
