882a545d8edb909bba0b06b62bd59e4d03186f5b
Taichi Ishitani <taichi730@gmail.com>
2023-08-08 23:46:09 +0900

    fix typo (#92, rggen/rggen#156)

--- lib/rggen/systemverilog/rtl/register_block/protocol/axi4lite.rb
+++ lib/rggen/systemverilog/rtl/register_block/protocol/axi4lite.rb
@@ -5,7 +5,7 @@ RgGen.define_list_item_feature(:register_block, :protocol, :axi4lite) do
     verify(:component) do
       error_condition { ![32, 64].include?(configuration.bus_width) }
       message do
-        'bus width eigher 32 bit or 64 bit is only supported: ' \
+        'bus width either 32 bit or 64 bit is only supported: ' \
         "#{configuration.bus_width}"
       end
     end
